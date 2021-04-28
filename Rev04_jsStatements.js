2a444bcf0808c9998ad6cb15ffceedb4dccfb96f
kemalgurler <kemgur52@gmail.com>
2021-04-28 18:00:03 -0400

    Apr28_06:00pm

---
+++ Rev04_jsStatements.js
@@ -0,0 +1,10 @@
+//IF-ELSE
+var email ="cangokce@gmail.com";
+var password = "123456";
+if(email=="cangokce@gmail.com" && password =="23456"){
+    console.log("Girisiniz basarili");
+}else if(emain=="admin@gmail.com"){
+    console.log("Admin olarak giris yapamazsiniz");
+}else{
+    console.log("Girisiniz basarisiz");  
+}
