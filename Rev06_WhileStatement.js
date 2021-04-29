4873c41a0607a05515556515ccd89bc5eb0149aa
kemalgurler <kemgur52@gmail.com>
2021-04-29 09:03:29 -0400

    Apr29_09:03am

--- Rev06_WhileStatement.js
+++ Rev06_WhileStatement.js
@@ -1,6 +1,23 @@
 var counter =0;
-while(counter<=10){
+while(counter<10){
     console.log("JavaScript");
+    document.write("JavaScript <br>");
+    counter=counter+1;
+    // Code injected by live-server 
+
+    //While dongusu ile carpma islemi yapmak
+    var counter1 =0;
+    var value =5;
+    var result = 0;
+
+    while(counter1<10){
+        result =result + value * counter1;
+        document.write(value * counter1 +"<br>")
+        counter1=counter1+1;
+    }
+    console.log(counter1+' x '+ value+' = '+result);
+
+
 
 
 }
---
+++ Rev07_DoWhileStatement.js
@@ -0,0 +1,15 @@
+var counter =0;
+// do{
+//     console.log(counter +" Javascript")
+//     counter++;
+// }while(counter<10);
+
+do{
+counter++;
+if(counter%3==0){
+    console.log(counter);
+}
+}while(counter<100);
+
+
+
