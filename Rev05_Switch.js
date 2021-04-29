4489cb9ac670e3fabe6babd8251a89e196f50ef0
kemalgurler <kemgur52@gmail.com>
2021-04-28 23:14:47 -0400

    Apr28_11:14pm

---
+++ Rev05_Switch.js
@@ -0,0 +1,13 @@
+var email = "cangokce@gmail.com";
+switch(email){
+    case "cangokce@gmail.com":
+        console.get("Emailiniz cangokce@gmail.com");
+        break;
+    case "Mcangokce@gmail.com":
+        console.get("Emailiniz cangokce@gmail.com");
+        break;  
+    default: 
+        console.get("Uymadi");     
+
+
+}
