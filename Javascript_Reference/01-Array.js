918417b63c1911798bf0689fee931f1339e85600
kemalgurler <kemgur52@gmail.com>
2021-05-24 22:42:18 -0400

    May24

---
+++ Javascript_Reference/01-Array.js
@@ -0,0 +1,5 @@
+// Array Object
+// The Array object is used to store multiple values in a single variable
+// Array nesnesi, tek bir değişkende birden çok değeri depolamak için kullanılır:
+var cars = ["Saab", "Volvo", "BMW"];
+
---
+++ Javascript_Reference/02-Boolean.js
@@ -0,0 +1,8 @@
+// JavaScript Booleans
+// JavaScript booleans can have one of two values: true or false.
+//JavaScript booleanları iki değerden birine sahip olabilir: true veya false
+
+// The Boolean() Function
+// You can use the Boolean() function to find out if an expression is true:
+//You can use the Boolean() function to find out if an expression is true:
+Boolean(10 > 9)     // returns true
