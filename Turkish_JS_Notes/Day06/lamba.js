7f4edb573a2cabd0a59cb0cbb66b67090c1b6f93
kemalgurler <kemgur52@gmail.com>
2021-06-23 12:52:48 -0400

    jun23

--- Turkish_JS_Notes/Day06/lamba.js
+++ Turkish_JS_Notes/Day06/lamba.js
@@ -4,3 +4,12 @@ document.querySelector(".on").onclick =  function(){
 document.querySelector(".off").onclick =  function(){
     document.querySelector(".resim").src = "./imgBulb/bulb off.jpeg";
 };
+//Resmin uzerine her geldiginde bulb_on resmini goster
+document.querySelector(".resim").addEventListener("mouseover", ()=>{
+    document.querySelector(".resim").src = "./imgBulb/bulb on.jpeg";
+} )
+
+//Resmin uzerinden ayrildiginda  bulb_off resmini goster
+document.querySelector(".resim").addEventListener("mouseout",function(){
+    document.querySelector(".resim").src = "./imgBulb/bulb off.jpeg"; 
+})
