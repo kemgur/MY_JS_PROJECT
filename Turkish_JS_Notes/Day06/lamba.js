1ac6a88d3c613d29ea07a1a2b81f06e0c2c7453f
kemalgurler <kemgur52@gmail.com>
2021-06-22 21:42:25 -0400

    Jun22_09:42

---
+++ Turkish_JS_Notes/Day06/lamba.js
@@ -0,0 +1,6 @@
+document.querySelector(".on").onclick =  function(){
+    document.querySelector(".resim").src = "./imgBulb/bulb on.jpeg";
+};
+document.querySelector(".off").onclick =  function(){
+    document.querySelector(".resim").src = "./imgBulb/bulb off.jpeg";
+};
